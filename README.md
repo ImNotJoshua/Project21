1748cf70def0391daae73edaa759e3e65506e306
ImNotJoshua <73602117+ImNotJoshua@users.noreply.github.com>
2020-11-26 19:39:18 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Project21
-In this we calculate the amount of damage taken by the wall when a bullet comes in contact with it
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,52 @@
+var bulllet,speed,weight,wall,thickness;
+
+function setup() {
+  createCanvas(1600,400);
+
+  speed=random(223,321);
+
+  weight=random(30,52);
+
+  bullet=createSprite(50,200,50,25);
+
+  thickness=random(22,83);
+
+  wall=createSprite(1200,200,thickness,height/2);
+  wall.shapeColor=color(80,80,80);
+
+  bullet.velocityX=speed;
+
+}
+
+function draw() {
+  background(255,255,255);  
+  
+  if(hasCollided(bullet,wall)) {
+
+    bullet.velocityX=0;
+
+    var dmg = 0.5*weight*speed*speed/(thickness*thickness*thickness);
+    if(dmg>10){
+      wall.shapeColor=color(250,0,0);
+    }
+    if(dmg<10){
+      wall.shapecolor=color(0,255,0);
+    }
+  }
+
+  drawSprites();
+}
+
+function hasCollided(lbullet,lwall) {
+
+  var bulletRightEdge=lbullet.x+lbullet.width;
+  var wallLeftEdge=lwall.x
+
+  if(bulletRightEdge>=wallLeftEdge) {
+
+    return true;
+  }
+  return false;
+}
+
+  
